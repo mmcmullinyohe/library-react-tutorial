efd65a8ef962925aff08e4ce1b42cf88d140b042
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-22 15:17:42 -0500

    featured file update

---
+++ src/components/Featured.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Featured = () => {
+    return (
+        <div>
+            Featured <span className="purple">Books</span>
+        </div>
+    );
+}
+
+export default Featured;
