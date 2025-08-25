55875d22f2851b313cc253b8bbedfcfc340b1684
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-25 12:42:43 -0500

    finished featured books

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -15,11 +15,9 @@ const Book = ({ book }) => {
         </a>
       </div>
       <div className="book__ratings">
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star-half-alt" />
+       {
+        new Array(5).fill(0).map((element, index) => <FontAwesomeIcon icon="star" key={index} />) 
+       }
       </div>
       <div className="book__price">
         {book.salePrice ? (
