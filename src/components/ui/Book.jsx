dd8a39c668f1424f552b0bdc0db849f68bab0f11
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-25 11:19:02 -0500

    updated sales and original price

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -2,31 +2,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Book = ({ book }) => {
-    return (
-                        <div className="book">
-                            <a href="">
-                             <figure className="book__img--wrapper">
-                                <img src={book.url} alt="" className="book__img" />
-                                </figure>   
-                            </a>
-                            <div className="book__title">
-                                <a href="/" className="book__title--link">
-                                    {book.title}
-                                </a>
-                            </div>
-                            <div className="book__ratings">
-                               <FontAwesomeIcon icon="star" />
-                               <FontAwesomeIcon icon="star" />
-                               <FontAwesomeIcon icon="star" />
-                               <FontAwesomeIcon icon="star" />
-                               <FontAwesomeIcon icon="star-half-alt" />
-                            </div>
-                            <div className="book__price">
-                                <span className="book__price--normal">${book.originalPrice}</span>
-                                ${book.salePrice}
-                            </div>
-                        </div>
-    );
-}
+  return (
+    <div className="book">
+      <a href="">
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </a>
+      <div className="book__title">
+        <a href="/" className="book__title--link">
+          {book.title}
+        </a>
+      </div>
+      <div className="book__ratings">
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star-half-alt" />
+      </div>
+      <div className="book__price">
+        {book.salePrice ? (
+          <>
+            <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>$
+            {book.salePrice.toFixed(2)}
+          </>
+        ) : (
+          <>${ book.originalPrice }</>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Book;
