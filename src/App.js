a5af132d60699144619c087bfb1299a4733bc0ce
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-27 10:22:42 -0500

    changing books

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import Home from './pages/Home';
 import Footer from './components/Footer'
 import Nav from './components/Nav';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import react from 'react';
+import Home from './pages/Home';
 import Books from "./pages/Books";
+import books from "./data";
 
 function App() {
   return (
     <Router> 
     <div className="App">
-      <Route />
       <Nav />
       <Route path="/" exact component={Home} />
-      <Route path="/books" component={Books} />
-      <Home />
+      <Route path="/books" render={() => <Books books={books} />}/>
       <Footer />
     </div>
     </Router>
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { books } from "../data";
-import Book from "../components/ui/Book";
 
-const Books = () => {
+const Books = ({ books: initialBooks }) => {
+    const [books, setBooks] = useState(initialBooks);
     return (
         <div id="books__body">
             <main id="books__main">
