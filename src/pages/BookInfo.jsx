5f1af1217304bef2d695d9530188357aa098b559
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-28 16:07:22 -0500

    book info sales

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import Rating from "../components/ui/Rating";
+import Price from "../components/ui/Price";
 
 
 const BookInfo = ({ books }) => {
@@ -12,13 +14,23 @@ const BookInfo = ({ books }) => {
                 <div className="row">
                     <div className="book__selected--top">
                         <Link to="/book" className="book__link">
+                        <FontAwesomeIcon icon="arrow-left" />
+                        </Link>
+                        <Link to="/books" className="book__link">
                         <h2 className="book__selected--title--top">Books</h2>
                         </Link>
                     </div>
                     <div className="book__selected">
                       <figure className="book__selected--figure">
                         <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--ig" />
-                        </figure>  
+                        </figure> 
+                        <div className="book__selected--description">
+                        <h2 className="book__selected--title">Cracking the Coding Interview</h2>
+                        <Rating rating="4.5"/>
+                        <div className="book__selected--price">
+                        <Price originalPrice={50} salePrice={20}/>
+                        </div>
+                        </div> 
                     </div>
                 </div>
             </div>
