52952a4b836af6278d47f2b79d94ae8e0cbbd22a
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-28 10:29:09 -0500

    edits

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -2,40 +2,51 @@ import React, { useState } from "react";
 import { books } from "../data";
 
 const Books = ({ books: initialBooks }) => {
-    const [books, setBooks] = useState(initialBooks);
-    function filterBooks(filter) {
-        if (filter === 'LOW TO HIGH') {
-            setBooks(books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))      
-        }
-
+  const [books, setBooks] = useState(initialBooks);
+  function filterBooks(filter) {
+    if (filter === "LOW TO HIGH") {
+      setBooks(
+        books.sort(
+          (a, b) =>
+            (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
+        )
+      );
     }
-    return (
-        <div id="books__body">
-            <main id="books__main">
-                <section>
-                    <div className="books__container">
-                        <div className="row">
-                            <div className="books__header">
-                                <h2 className="section__title books__header--title">All Books</h2>
-                                <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
-                                    <option value="DEFAULT" disabled>Sort</option>
-                                    <option value="LOW TO HIGH">Price, Low to High</option>
-                                    <option value="HIGH TO LOW">Price, High to Low</option>
-                                    <option value="RATING">Rating</option>
-                                </select>
-                            </div>
-                            <div className="books">
-                              {
-                                books.map((book) => (
-                                <Book book={book} key={book.id} />
-                                ))}  
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </div>
-    );
+  }
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <section>
+          <div className="books__container">
+            <div className="row">
+              <div className="books__header">
+                <h2 className="section__title books__header--title">
+                  All Books
+                </h2>
+                <select
+                  id="filter"
+                  defaultValue="DEFAULT"
+                  onChange={(event) => filterBooks(event.target.value)}
+                >
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="LOW TO HIGH">Price, Low to High</option>
+                  <option value="HIGH TO LOW">Price, High to Low</option>
+                  <option value="RATING">Rating</option>
+                </select>
+              </div>
+              <div className="books">
+                {books.map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Books;
